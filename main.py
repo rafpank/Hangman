20c3fe83b6356df622554b3956cbbec606d4839b
rafpank <portaleobs@gmail.com>
2024-05-16 15:13:33 +0200

    Added greetings

---
+++ main.py
@@ -0,0 +1,6 @@
+#My first project in Python - Game of hangman
+
+imie = str(input("Cześć, jak masz na imie? "))
+print("Witaj", imie, ". \nZapraszam Cię do gry")
+      
+zgadywane_haslo = 'KOLEJKA'
