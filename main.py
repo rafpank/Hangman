90d445cce96e5f6c5b7f7cd08ee812269be9b40c
rafpank <portaleobs@gmail.com>
2024-05-16 15:39:42 +0200

    Added module iterating over a password and displaying the number of underlines on this basis

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #My first project in Python - Game of hangman
 
 imie = str(input("Cześć, jak masz na imie? "))
-print("Witaj", imie, ". \nZapraszam Cię do gry")
+print(f'Witaj {imie}.\nZapraszam Cię do gry.\n')
       
 zgadywane_haslo = 'KOLEJKA'
+
+#Tablica wypisująca znaki podkreślenia czyli pętla która będzie się iterowała po zgadywanym haśle i wpisywała podkreslenia do listy 
+
+podreslenia = list(zgadywane_haslo)
+
+for podloga in range(len(podreslenia)):
+    podreslenia[podloga] = "_ "
+print("Oto miejsce na nasze hasło: ",podreslenia)
