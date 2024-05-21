1d50cde2c29c37bb484c94c9163b2ee6e92d2b12
rafpank <portaleobs@gmail.com>
2024-05-21 12:53:17 +0200

    added the main function of the game based on the while loop

--- main.py
+++ main.py
@@ -42,6 +42,37 @@ def graf(lives):
     ]
     return gallows_graf[lives]
 
+def game(words: list, lives: int):
+    word_to_guess = random.choice(words).upper()
+    display_word = "_ " * len(word_to_guess)
+    good_guesses = set()
+
+    while lives > 0 and "_" in display_word:
+        print(f"Try guessing this word: {display_word}")
+        print(graf(lives)) 
+        letter = input("Try guessing the letter: ").upper()
+        if letter in word_to_guess:
+            good_guesses.add(letter)
+            display_word = ""
+            for character in word_to_guess:
+                if character in good_guesses:
+                    display_word += character
+                else:
+                    display_word += "_"
+                display_word += " "
+        else:
+            lives -= 1
+            if lives > 0:
+                print(f"You miss! but still you have {lives} tries")
+            else:
+                print("GAME OVER")
+                print(graf(lives))
+    if lives > 0:
+        print("You are the best! \n You won!!!")
+    else: 
+        print("You lost the game :(")
+
+
 def main():
     countries = [ 
         "France", "Germany", "Italy", "Spain", "Greece", "Austria", "Hungary", "Poland", "Portugal"
@@ -49,6 +80,9 @@ def main():
     lives = 3
 
     answer = input("Do you want to start new game? Y/N ")
+    while answer.lower() == "y":
+        game(countries, lives)
+        answer = input("Do you want to start new game? Y/N ")
 
 
 main()
